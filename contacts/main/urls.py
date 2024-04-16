b4a45d8c0d6086d6f08e708376a13ebccf4332c7
johnpii <roman.shubin2001@mail.ru>
2024-04-16 08:47:13 +0400

    feat: some cleaning

--- contacts/main/urls.py
+++ contacts/main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('clients/', views.client_list, name='client_list'),
-    path('clients/add/', views.add_client, name='add_client'),
-    path('clients/edit/<int:id>/', views.edit_client, name='edit_client'),
-    path('clients/delete/<int:id>/', views.delete_client, name='delete_client'),
+    path('', views.client_list, name='client_list'),
+    path('/add/', views.add_client, name='add_client'),
+    path('/edit/<int:id>/', views.edit_client, name='edit_client'),
+    path('/delete/<int:id>/', views.delete_client, name='delete_client'),
 ]
